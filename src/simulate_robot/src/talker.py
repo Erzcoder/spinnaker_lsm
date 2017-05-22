480f680b8fa2a9f3b0b138605973dadadc9d19d9
Erzcoder <n.berberich@gmail.com>
2017-05-22 16:31:22 +0200

    gazebo arm go to position

--- src/simulate_robot/src/talker.py
+++ src/simulate_robot/src/talker.py
@@ -6,34 +6,45 @@ from control_msgs.msg import JointTrajectoryControllerState
 from control_msgs.msg import FollowJointTrajectoryActionGoal
 from trajectory_msgs.msg import JointTrajectoryPoint
 import math
+import numpy as np
 
 
 def talker():
+    rospy.loginfo('starting the talker node')
+    global counter
     pub = rospy.Publisher('chatter', String, queue_size=10)
     #pub2 = rospy.Publisher('test_trajectory',JointTrajectoryControllerState, queue_size=10)
     rospy.init_node('talker', anonymous=True)
-    rate = rospy.Rate(100) # 10hz
-    while not rospy.is_shutdown():
-        hello_str = "hello world %s" % rospy.get_time()
-        #rospy.loginfo(hello_str)
-        pub.publish(hello_str)
+    rate = rospy.Rate(10) # 10hz
+    #while not rospy.is_shutdown():
+    #    hello_str = "hello world %s" % rospy.get_time()
+    #    #rospy.loginfo(hello_str)
+    #    pub.publish(hello_str)
 
     pub3= rospy.Publisher('/arm_controller/follow_joint_trajectory/goal', FollowJointTrajectoryActionGoal, queue_size=10)
     
 
 
     # we need a discretized sinusoid
-    for i in range(100):
-	command = FollowJointTrajectoryActionGoal()
-   	command.header.stamp = rospy.Time.now()
-   	command.goal.trajectory.joint_names = ['elbow']
-        point = JointTrajectoryPoint()
-        point.positions = [math.sin(i/4)*math.pi] 
-	# in which range is sinus defined?
-        point.time_from_start = rospy.Duration(i)
-	command.goal.trajectory.points.append(point)
-    	pub3.publish(command)
-    	rospy.loginfo('=====send command %r', command.goal.trajectory.points[0])
+    # x = np.linspace(0, 20, 100)
+    # traj=np.sin(x)
+
+    rospy.sleep(2)
+
+    command = FollowJointTrajectoryActionGoal()
+    command.header.stamp = rospy.Time.now()
+    command.goal.trajectory.joint_names = ['elbow']
+    point = JointTrajectoryPoint()
+    point.positions=[math.pi/2.]
+    point.velocities=[2]
+    #point.positions = [math.sin(counter/4)*math.pi] 
+    # in which range is sinus defined?
+    point.time_from_start = rospy.Duration(10)
+    command.goal.trajectory.points.append(point)
+
+
+    pub3.publish(command)
+    rospy.loginfo('=====send command %r', command.goal.trajectory.points[0])
 
     
 
@@ -44,6 +55,9 @@ def talker():
 
 if __name__ == '__main__':
     try:
+        global counter
+        counter=0
         talker()
+
     except rospy.ROSInterruptException:
         pass
