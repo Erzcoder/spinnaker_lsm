a85fa90fd85a5967d1de10c608cd637453e7755f
Erzcoder <n.berberich@gmail.com>
2017-05-31 11:02:37 +0200

    optimizing network

--- src/simulate_robot/src/talker.py
+++ src/simulate_robot/src/talker.py
@@ -12,42 +12,46 @@ import numpy as np
 def talker():
     rospy.loginfo('starting the talker node')
     global counter
-    pub = rospy.Publisher('chatter', String, queue_size=10)
-    #pub2 = rospy.Publisher('test_trajectory',JointTrajectoryControllerState, queue_size=10)
     rospy.init_node('talker', anonymous=True)
     rate = rospy.Rate(10) # 10hz
-    #while not rospy.is_shutdown():
-    #    hello_str = "hello world %s" % rospy.get_time()
-    #    #rospy.loginfo(hello_str)
-    #    pub.publish(hello_str)
+    
 
-    pub3= rospy.Publisher('/arm_controller/follow_joint_trajectory/goal', FollowJointTrajectoryActionGoal, queue_size=10)
+    pub= rospy.Publisher('/arm_controller/follow_joint_trajectory/goal', FollowJointTrajectoryActionGoal, queue_size=10)
     
 
 
     # we need a discretized sinusoid
-    # x = np.linspace(0, 20, 100)
-    # traj=np.sin(x)
+    x = np.linspace(0, 6, 10)
+    traj=np.sin(x)
 
+    
     rospy.sleep(2)
 
     command = FollowJointTrajectoryActionGoal()
     command.header.stamp = rospy.Time.now()
     command.goal.trajectory.joint_names = ['elbow']
     point = JointTrajectoryPoint()
-    point.positions=[math.pi/2.]
-    point.velocities=[2]
+    point.positions=[math.pi/2]
+    point.velocities=[1.5] #0.5
     #point.positions = [math.sin(counter/4)*math.pi] 
     # in which range is sinus defined?
     point.time_from_start = rospy.Duration(10)
     command.goal.trajectory.points.append(point)
+    point.positions=[0]
+    point.velocities=[1.5] #0.5
+    #point.positions = [math.sin(counter/4)*math.pi] 
+    # in which range is sinus defined?
+    point.time_from_start = rospy.Duration(20)
+    command.goal.trajectory.points.append(point)
 
 
-    pub3.publish(command)
+
+    pub.publish(command)
     rospy.loginfo('=====send command %r', command.goal.trajectory.points[0])
 
     
-
+    rospy.sleep(2)
+    counter+=1
 
     rate.sleep()
 
