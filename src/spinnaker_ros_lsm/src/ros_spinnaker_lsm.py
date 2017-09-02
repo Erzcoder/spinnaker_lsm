a2df1048a85b5d78a8e8a4f01ada6abdba16dc6c
letrend <simon.trendel@tum.de>
2017-09-02 23:19:59 +0200

    save before catkin_make of spinnaker_ros_lsm

--- src/spinnaker_ros_lsm/src/ros_spinnaker_lsm.py
+++ src/spinnaker_ros_lsm/src/ros_spinnaker_lsm.py
@@ -2,11 +2,16 @@
 # -*- coding: utf-8 -*-
 
 """
-@author Stephan Reith
-@date 	31.08.2016
-This is a simple example to demonstrate how the ROS Spinnaker Interface can be used.
-You will also need a ROS Listener and a ROS Talker to send and receive data.
-Make sure they communicate over the same ROS topics and std_msgs.Int64 ROS Messages used in here.
+@author Nicolas Berberich
+@date 	02.09.2017
+
+This script takes the ROS-messages from the rostopic /to_spinnaker and sends them to the input neuron
+population. This input population is then connected to the reservoir of the Liquid State Machine which 
+itself is connected to the two readout neurons. The spikes occuring in the readout neurons are then send 
+over the neural decoder (neural transfer function) to the rostopic /from_spinnaker. 
+The message type send is std_msgs/Int64.
+
+Credits to Stephan Reith for developing the ROS-SpiNNaker interface
 """
 
 import spynnaker.pyNN as pynn
@@ -17,41 +22,47 @@ from ros_spinnaker_interface import SpikeSourcePoisson
 from ros_spinnaker_interface import SpikeSinkSmoothing
 
 
-ts = 0.1
-n_neurons = 1
-simulation_time = 10000  # ms
+ts = 0.1				 # simulation timestep in ms
+simulation_time = 3000  # ms
+
+n_input_neurons   = 1
+n_readout_neurons = 2	 # 
+
+
 
 
 pynn.setup(timestep=ts, min_delay=ts, max_delay=2.0*ts)
 
 
-pop = pynn.Population(size=n_neurons, cellclass=pynn.IF_curr_exp, cellparams={}, label='pop')
+# set up the readout population
+readout_neurons = pynn.Population(size=n_readout_neurons, cellclass=pynn.IF_curr_exp, cellparams={}, label='readout_neurons')
 
 
 # The ROS_Spinnaker_Interface just needs to be initialised. The following parameters are possible:
 ros_interface = ROS_Spinnaker_Interface(
-        n_neurons_source=n_neurons,                 # number of neurons of the injector population
+        n_neurons_source=n_input_neurons,                 # number of neurons of the injector population
         Spike_Source_Class=SpikeSourcePoisson,   # the transfer function ROS Input -> Spikes you want to use.
         Spike_Sink_Class=SpikeSinkSmoothing,     # the transfer function Spikes -> ROS Output you want to use.
                                                     # You can choose from the transfer_functions module
                                                     # or write one yourself.
-        output_population=pop,                      # the pynn population you wish to receive the
+        output_population=readout_neurons,                      # the pynn population you wish to receive the
                                                     # live spikes from.
         ros_topic_send='to_spinnaker',              # the ROS topic used for the incoming ROS values.
         ros_topic_recv='from_spinnaker',            # the ROS topic used for the outgoing ROS values.
         clk_rate=1000,                              # mainloop clock (update) rate in Hz.
         ros_output_rate=10)                         # number of ROS messages send out per second.
 
+
 # Build your network, run the simulation and optionally record the spikes and voltages.
-pynn.Projection(ros_interface, pop, pynn.OneToOneConnector(weights=5, delays=1))
+pynn.Projection(ros_interface, readout_neurons, pynn.OneToOneConnector(weights=5, delays=1))
 
 
-pop.record()
-pop.record_v()
+readout_neurons.record()
+readout_neurons.record_v()
 
 pynn.run(simulation_time)
 
-spikes = pop.getSpikes()
+spikes = readout_neurons.getSpikes()
 
 pynn.end()
 
